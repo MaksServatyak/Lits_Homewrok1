6803cf0864c6088ba6383aa04c8e33a1b24c7b35
Andrii <andriy.servatiak@gmail.com>
2019-09-30 21:27:53 +0300

    first homework

---
+++ src/AppMain.java
@@ -0,0 +1,23 @@
+public class AppMain {
+
+    public static void main(String[] args) {
+        var a = 12;
+        var b = 3;
+        var d = -5;
+        var c = 86.3;
+        var e = 0;
+
+
+        var formule1 = (a + b) / (d - b) * 2;
+        var formule2 = a + 2 * 2 - 12 / a * (b - c);
+        var formule3 = b + 11 * c - a;
+        var formule4 = a * (c + b + d) - e / 1 + a;
+        var formule5 = a + b * (a - b) / 12;
+        System.out.println(formule1);
+        System.out.println(formule2);
+        System.out.println(formule3);
+        System.out.println(formule4);
+        System.out.println(formule5);
+
+    }
+}
